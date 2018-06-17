20509918126e6fe4fdddbf84fc01cdbc43b966bf
umitliguler <info.umitguler@gmail.com>
2018-06-17 12:19:30 +0300

    first

---
+++ AttributeKullanmak/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeKullanmak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DebugMethod();
+            //OldMethod();
+
+            Console.ReadKey();
+
+        }
+
+        [Obsolete("Bu metod artık kullanılmıyor. Bunun yerine ... metodunu kullanın.", true)]
+        public static void OldMethod()
+        {
+            Console.WriteLine("OldMethod çalıştırıldı");
+        }
+
+        [Conditional("DEBUG")]
+        public static void DebugMethod()
+        {
+            Console.WriteLine("Debug Metod Çalıştı");
+        }
+
+    }
+}
